fc11912fd3e8b33d38479fc83013f213b185ad2f
Warren Haskins <warrenhaskins@gmail.com>
2013-05-22 00:06:12 +0100

    First changes for version 2

--- src/choice.js
+++ src/choice.js
@@ -13,54 +13,78 @@
 	var 	pluginName = 'choice';
 
 	function Plugin( element ) {
-		this.element = element;
+		this.el = $(element);
 		this.init();
 	}
 
-	Plugin.prototype.init = function () {
+	Plugin.prototype = {
 	
-		var	thiz = $(this.element),
-			selection = thiz.children(),
-			cont = thiz.wrap('<div class="Choice" />').parent(),
-			input = $('<input />').attr({"name" : thiz.attr("name"), "type" : "hidden"}).appendTo(cont),
-			list = $(cont).append('<ul class="ListChoice" />').find('ul').hide(),
-			current = cont.prepend('<span class="ChoiceCurrent" />').find('span'),
-			firstValue = selection.eq(0).val() || selection.eq(0).text(),
-			firstText = selection.eq(0).text();
+		// var	thiz = $(this.element),
+		// 	selection = thiz.children(),
+		// 	cont = thiz.wrap('<div class="Choice" />').parent(),
+		// 	input = $('<input />').attr({"name" : thiz.attr("name"), "type" : "hidden"}).appendTo(cont),
+		// 	list = $(cont).append('<ul class="ListChoice" />').find('ul').hide(),
+		// 	current = cont.prepend('<span class="ChoiceCurrent" />').find('span'),
+		// 	firstValue = selection.eq(0).val() || selection.eq(0).text(),
+		// 	firstText = selection.eq(0).text();
 
-		thiz.remove();
+		// thiz.remove();
 
-		current.text(firstText);
-		$(input).val(firstValue);
+		// current.text(firstText);
+		// $(input).val(firstValue);
 
-		selection.each(function(index){
-			if(!$(this).attr("selected")) {
-				$(list).append('<li data-item="' + index + ' ">' + $(this).text() + '</li>');
-			} else {
-				$(list).append('<li data-item="' + index + ' ">' + $(this).text() + '</li>');
-				current.text($(this).text());
-				$(input).val($(this).val() || $(this).text());
-			}
-		});
+		// selection.each(function(index){
+		// 	if(!$(this).attr("selected")) {
+		// 		$(list).append('<li data-item="' + index + ' ">' + $(this).text() + '</li>');
+		// 	} else {
+		// 		$(list).append('<li data-item="' + index + ' ">' + $(this).text() + '</li>');
+		// 		current.text($(this).text());
+		// 		$(input).val($(this).val() || $(this).text());
+		// 	}
+		// });
 
-		current.click(function(){
-			list.toggle();
-		});
+		// current.click(function(){
+		// 	list.toggle();
+		// });
 
-		list.find('li').click(function(){
-			var 	id = $(this).data("item"),
-				value = selection.eq(id).val() || selection.eq(id).text(),
-				text = selection.eq(id).text();
-			$(input).val(value);
-			current.text(text);
-			list.hide();
-		});
+		// list.find('li').click(function(){
+		// 	var 	id = $(this).data("item"),
+		// 		value = selection.eq(id).val() || selection.eq(id).text(),
+		// 		text = selection.eq(id).text();
+		// 	$(input).val(value);
+		// 	current.text(text);
+		// 	list.hide();
+		// });
 
-		$(document).mouseup(function (e){
-		    if (cont.has(e.target).length === 0) cont.find(list).hide();
-		});
+		// $(document).on('mouseup', function (e){
+		//     if (cont.has(e.target).length === 0) cont.find(list).hide();
+		// });
+
+		init : function() {
+				this.el.hide();
+				this.children = this.el.children(),
+				this.container = this.el.wrap('<div class="ChoiceJS" />').parent(),
+				this.hiddenInput = $('<input />').prop({ 'name' : this.el.prop('name'), 'type' : 'hidden' }).appendTo(this.container),
+				this.list = $('<ul class="ChoiceList" />').appendTo(this.container),
+				this.current = $('<div />').prop('class', 'ChoiceCurrent').prependTo(this.container);
+
+				this.children.each($.proxy(this.item, this));
+		},
+
+		item : function(index, val) {
+			var item = $('<li />').text(val.value);
+			if($(val).prop('selected')) this.set(val);
+			item.prop('data-index', index).appendTo(this.list);
+		},
+
+		set : function(val) {
+			this.current.text(val.value);
+			this.hiddenInput.val( val.value || val.innerText )
+		}
 
 	};
+
+
 
 	$.fn[pluginName] = function () {
 		return this.each(function () {
