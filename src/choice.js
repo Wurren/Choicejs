907acb7869a1ee3a315c3c47b52765c02c40f92f
Warren Haskins <warrenhaskins@gmail.com>
2013-05-24 19:47:38 +0100

    Changed Events, removed old code

--- src/choice.js
+++ src/choice.js
@@ -18,49 +18,9 @@
 	}
 
 	Plugin.prototype = {
-	
-		// var	thiz = $(this.element),
-		// 	selection = thiz.children(),
-		// 	cont = thiz.wrap('<div class="Choice" />').parent(),
-		// 	input = $('<input />').attr({"name" : thiz.attr("name"), "type" : "hidden"}).appendTo(cont),
-		// 	list = $(cont).append('<ul class="ListChoice" />').find('ul').hide(),
-		// 	current = cont.prepend('<span class="ChoiceCurrent" />').find('span'),
-		// 	firstValue = selection.eq(0).val() || selection.eq(0).text(),
-		// 	firstText = selection.eq(0).text();
-
-		// thiz.remove();
-
-		// current.text(firstText);
-		// $(input).val(firstValue);
-
-		// selection.each(function(index){
-		// 	if(!$(this).attr("selected")) {
-		// 		$(list).append('<li data-item="' + index + ' ">' + $(this).text() + '</li>');
-		// 	} else {
-		// 		$(list).append('<li data-item="' + index + ' ">' + $(this).text() + '</li>');
-		// 		current.text($(this).text());
-		// 		$(input).val($(this).val() || $(this).text());
-		// 	}
-		// });
-
-		// current.click(function(){
-		// 	list.toggle();
-		// });
-
-		// list.find('li').click(function(){
-		// 	var 	id = $(this).data("item"),
-		// 		value = selection.eq(id).val() || selection.eq(id).text(),
-		// 		text = selection.eq(id).text();
-		// 	$(input).val(value);
-		// 	current.text(text);
-		// 	list.hide();
-		// });
-
-		// $(document).on('mouseup', function (e){
-		//     if (cont.has(e.target).length === 0) cont.find(list).hide();
-		// });
 
 		init : function() {
+
 			this.el.hide();
 			this.children = this.el.children(),
 			this.container = this.el.wrap('<div class="ChoiceJS" />').parent(),
@@ -78,22 +38,15 @@
 				if (that.container.has(e.target).length === 0) that.list.hide();
 			});
 
-			this.current.on('click',function(){
+			this.current.on('click', function(){
 				that.list.toggle();
-				var i = that.list.children();
-				var h = $(i[0]).innerHeight() * 3;
-				console.log(h);
-				that.list.height(h);
 			});
 
-			// list.find('li').click(function(){
-			// 	var 	id = $(this).data("item"),
-			// 		value = selection.eq(id).val() || selection.eq(id).text(),
-			// 		text = selection.eq(id).text();
-			// 	$(input).val(value);
-			// 	current.text(text);
-			// 	list.hide();
-			// });
+			this.current.one('click', function() {
+				var	i = that.list.children(),
+					h = $(i[0]).innerHeight() * 3;
+				if(i.length > 3) that.list.height(h);
+			});
 
 			this.list.on('click', 'li', $.proxy(this.change, this));
 
